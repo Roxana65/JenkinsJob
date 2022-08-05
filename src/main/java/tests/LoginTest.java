a1698ca874f7236cb0de2fe44084bafe9a4365b4
unknown <you@example.com>
2022-08-05 21:40:59 +0300

    lo

--- src/main/java/tests/LoginTest.java
+++ src/main/java/tests/LoginTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import static org.testng.Assert.assertTrue;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -14,17 +15,21 @@ import utils.BaseTest;
 public class LoginTest extends BaseTest{
 	
 	@Parameters({"user", "pass"})
-	@Test
-	public void loginTest(String username, String password) {
+	@Test(priority=0)
+	public void loginTest(String username, String password) throws InterruptedException {
 		NavigationMenuPage navMenu = new NavigationMenuPage(driver);
 		navMenu.navigateTo(navMenu.loginLink);
 		//assertTrue(false);
 		LoginPage loginPage =  new LoginPage(driver);
 		loginPage.loginInApp(username, password);
+		Thread.sleep(3000);
+
+		driver.findElement(By.xpath("//li[@class='menu_user_logout']")).click();
+		Thread.sleep(3000);
 		
 		
 	}
-	@Test
+	@Test(priority=1)
 	public void loginTest2() {
 		NavigationMenuPage navMenu = new NavigationMenuPage(driver);
 		navMenu.navigateTo(navMenu.loginLink);
