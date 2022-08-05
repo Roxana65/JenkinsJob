a293a475941b439c8bda5424b35618300d3ea3bc
unknown <you@example.com>
2022-08-05 21:44:47 +0300

    ll

--- src/main/java/utils/BaseTest.java
+++ src/main/java/utils/BaseTest.java
@@ -64,7 +64,7 @@ public class BaseTest {
 		
 		
 		
-		driver.manage().window().maximize();
+		//driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 		driver.manage().window().setSize(new Dimension(1440, 900));
